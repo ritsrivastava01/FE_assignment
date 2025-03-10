6579ac43bfe7b76d4f6f3947d117e63934260858
Ritesh <ritsrivastava@gmail.com>
2025-03-10 12:34:29 +0100

    Refactor: enhance tests for BasePetSchema validation

--- src/schemas/pets.test.ts
+++ src/schemas/pets.test.ts
@@ -1,7 +1,8 @@
 import { BasePetSchema } from './pets';
 
 describe('Pet Schemas', () => {
-  it('should validate a valid BasePetSchema object', () => {
+  it('should validate a BasePetSchema object', () => {
+    // Arrange
     const validPet = {
       id: 1,
       dateAdded: '01-11-2022',
@@ -12,7 +13,10 @@ describe('Pet Schemas', () => {
       birthYear: 2018,
     };
 
+    // Act
     const result = BasePetSchema.safeParse(validPet);
+
+    // Assert
     expect(result.success).toBe(true);
     if (result.success) {
       expect(result.data.dateAdded).toBeInstanceOf(Date);
@@ -22,44 +26,61 @@ describe('Pet Schemas', () => {
 
   describe('Error handling', () => {
     it('should throw an error when the date is invalid', () => {
+      // Arrange
       const invalidPet = {
         id: 1,
         dateAdded: 'invalid-date',
         name: 'Buddy',
       };
+
+      // Act
       const result = BasePetSchema.safeParse(invalidPet);
+
+      // Assert
       expect(result.success).toBe(false);
       if (!result.success) {
         expect(result.error.issues[0].message).toBe('Invalid Date');
       }
     });
 
-    it('should valid BasePetSchema object', () => {
+    it('should validate invalid BasePetSchema object', () => {
+      // Arrange
       const invalidPet = {
-        id: 'one', // id should be a number
-        dateAdded: '01-01-2020', // Wrong date format
+        id: 'one',
+        dateAdded: '01-01-2020',
         name: 'Buddy',
         photoUrl: 'http://example.com/photo.jpg',
         species: 'Dog',
         available: 'yes',
         birthYear: 2018,
       };
+
+      // Act
       const result = BasePetSchema.safeParse(invalidPet);
+
+      // Assert
       expect(result.success).toBe(false);
     });
 
     it('should validate the species enum', () => {
+      // Arrange
       const invalidPet = {
-        id: 'one', // id should be a number
-        dateAdded: '01-01-2020', // Wrong date format
+        id: 1,
+        dateAdded: '01-01-2020',
         name: 'Buddy',
         photoUrl: 'http://example.com/photo.jpg',
         species: 'snake',
         available: 'yes',
         birthYear: 2018,
       };
+
+      // Act
       const result = BasePetSchema.safeParse(invalidPet);
+
+      // Assert
+      console.log(result.error);
       expect(result.success).toBe(false);
+      expect(result.error?.message).contain('Invalid enum value. Expected');
     });
   });
 });
